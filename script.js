1233e61110ebfcc7b8575b26c9c2aceb35fa03b3
skgough <44104194+skgough@users.noreply.github.com>
2021-05-11 23:42:31 -0400

    layout polish

--- script.js
+++ script.js
@@ -4,16 +4,24 @@ window.carousel = new Swipe(element, {
     draggable: true,
     autoRestart: false,
     continuous: false,
-    disableScroll: true,
+    disableScroll: false,
     stopPropagation: true,
     callback: function (index) {
-        let buttons = Array.from(document.querySelectorAll('button'));
-        let previous = document.querySelector('button.selected')
-            previous.classList.remove('selected')
-        buttons[index].classList.add('selected')
+        let buttons = Array.from(document.querySelectorAll('.gotos button'))
+        let current = buttons[index]
+        let parent = current.parentElement
+        let previous = document.querySelector('.gotos button.selected')
+        let currentWidth = current.getBoundingClientRect().width
+        let parentWidth = parent.getBoundingClientRect().width
+        let scrollPosition = (current.offsetLeft - parent.offsetLeft) + (currentWidth/2 - parentWidth/2)
+        
+        previous.classList.remove('selected')
+        current.classList.add('selected')
+               
+        buttons[index].parentElement.scrollTo(scrollPosition,0)
     }
 });
-let buttons = Array.from(document.querySelectorAll('button'));
+let buttons = Array.from(document.querySelectorAll('button'))
 for (const index in buttons) {
     buttons[index].addEventListener('click', () => {
         carousel.slide(index)
